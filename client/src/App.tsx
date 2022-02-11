2dd93fef5e37c52e4d3a37acc0483da374754cff
WooJuJang <wooju.jang@vatech.com>
2022-02-11 15:55:08 +0900

    testccommit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      hi i am branch1!!<div>after revert1 try</div>
-      <div>thrid commit1</div>
-      <div>thrid commit2</div>
-      <div>thrid commit3</div>
-      <div>last1</div>
-    </div>
-  );
+  return <div className="App">...test</div>;
 }
 
 export default App;
