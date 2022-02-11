4703b7349aa051bac5eee4ff1f4d86328cf49102
WooJuJang <wooju.jang@vatech.com>
2022-02-11 11:52:22 +0900

    after revert commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">hi i am branch1!!</div>;
+  return (
+    <div className="App">
+      hi i am branch1!!<div>after revert1 try</div>
+    </div>
+  );
 }
 
 export default App;
