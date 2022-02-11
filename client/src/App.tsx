5e57d4173bad038686e77ba84468af37a9ae0c95
WooJuJang <wooju.jang@vatech.com>
2022-02-11 12:21:12 +0900

    thrid commit1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       hi i am branch1!!<div>after revert1 try</div>
-      <div>thrid commit</div>
+      <div>thrid commit1</div>
     </div>
   );
 }
