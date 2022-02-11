dd9920f47b35384fd15c3f1578e4c7bf68adf339
WooJuJang <wooju.jang@vatech.com>
2022-02-11 15:57:31 +0900

    add commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App">...test</div>;
+  return (
+    <div className="App">
+      ...test<div>add somthing</div>
+    </div>
+  );
 }
 
 export default App;
